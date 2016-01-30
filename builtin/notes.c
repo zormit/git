b8e751930bcc8992084313762272dfed6b408bef
Moritz Neeb <lists@moritzneeb.de>
2016-01-30 14:48:23 +0100

    notes: read copied notes with strbuf_getline()
    The notes are copied from stdin. They should only contain SHA1s... Not
    spaces. CR could be there, because the file/the data from stdin could
    have been written via an editor that adds them.
    
    The notes that are copied from stdin are trimmed with strbuf_rtrim() after
    splitting by ' '. There is thus no logic expecting CR, so strbuf_getline_lf()
    can be replaced by its CRLF counterpart.

--- builtin/notes.c
+++ builtin/notes.c
@@ -292,7 +292,7 @@ static int notes_copy_from_stdin(int force, const char *rewrite_cmd)
 		t = &default_notes_tree;
 	}
 
-	while (strbuf_getline_lf(&buf, stdin) != EOF) {
+	while (strbuf_getline(&buf, stdin) != EOF) {
 		struct object_id from_obj, to_obj;
 		struct strbuf **split;
 		int err;
@@ -301,7 +301,6 @@ static int notes_copy_from_stdin(int force, const char *rewrite_cmd)
 		if (!split[0] || !split[1])
 			die(_("malformed input line: '%s'."), buf.buf);
 		strbuf_rtrim(split[0]);
-		strbuf_rtrim(split[1]);
 		if (get_oid(split[0]->buf, &from_obj))
 			die(_("failed to resolve '%s' as a valid ref."), split[0]->buf);
 		if (get_oid(split[1]->buf, &to_obj))
